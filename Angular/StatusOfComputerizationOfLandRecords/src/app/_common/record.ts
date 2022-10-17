be5bad34a304eb290063044f7bb36683033185fc
Nikhil8bph <nikhil8.bph@gmail.com>
2022-10-17 11:40:50 +0530

    status of computerization of land records

---
+++ Angular/StatusOfComputerizationOfLandRecords/src/app/_common/record.ts
@@ -0,0 +1,7 @@
+export class Record {
+    sl_no_: string;
+    state_ut: string;
+    computerization_of_registration: string;
+    e_stamping: string;
+    integration_of_land_records_and_registration: string;
+}
